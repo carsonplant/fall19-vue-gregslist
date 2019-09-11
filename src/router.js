5aba8224bbde41c3fdc948e376ef9856e896ae1c
Carson Plant <carsonplant22@gmail.com>
2019-09-11 16:48:28 -0600

    updated router with jobs routes

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Cars from './views/Cars.vue'
+import Jobs from './views/Jobs.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/cars',
       name: 'cars',
       component: Cars
+    },
+    {
+      path: '/jobs',
+      name: 'jobs',
+      component: Jobs
     },
     {
       path: '/about',
@@ -29,6 +35,16 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: function () {
         return import(/* webpackChunkName: "car" */ './views/Car.vue')
+      }
+    },
+    {
+      path: '/jobs/:jobId',
+      name: 'job',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: function () {
+        return import(/* webpackChunkName: "car" */ './views/Job.vue')
       }
     },
     {
